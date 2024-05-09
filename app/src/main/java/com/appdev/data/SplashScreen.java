5863b412ba5a6aa04fe2460b9788db16876be62e
Jon0oo <jonobamxd11@gmail.com>
2024-05-09 19:31:06 +0200

    updated splash screen

--- app/src/main/java/com/appdev/data/SplashScreen.java
+++ app/src/main/java/com/appdev/data/SplashScreen.java
@@ -1,34 +1,76 @@
 package com.appdev.data;
 
 import android.content.Intent;
-import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
-import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.ViewPropertyAnimator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.ColorInt;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
+
 import java.util.Objects;
 
 
 public class SplashScreen extends AppCompatActivity {
-    private ImageView animationview;
+
     private AnimatedVectorDrawable animation;
+
+    private int currentImageIndex = 1;
+    private int[] imageResources = {
+            R.drawable.data_000,
+            R.drawable.data_001,
+            R.drawable.data_002,
+            R.drawable.data_003,
+            R.drawable.data_004,
+            R.drawable.data_005,
+            R.drawable.data_006,
+            R.drawable.data_007,
+            R.drawable.data_008,
+            R.drawable.data_009,
+            R.drawable.data_010,
+            R.drawable.data_011,
+            R.drawable.data_012,
+            R.drawable.data_013,
+            R.drawable.data_014,
+            R.drawable.data_015,
+            R.drawable.data_016,
+            R.drawable.data_017,
+            R.drawable.data_018,
+            R.drawable.data_019,
+            R.drawable.data_020,
+            R.drawable.data_021,
+            R.drawable.data_022,
+            R.drawable.data_023,
+            R.drawable.data_024,
+            R.drawable.data_025,
+            R.drawable.data_026,
+            R.drawable.data_027,
+            R.drawable.data_029,
+            R.drawable.data_030,
+            R.drawable.data_031,
+            R.drawable.data_032,
+            R.drawable.data_033,
+            R.drawable.data_034,
+            R.drawable.data_035,
+            R.drawable.data_036,
+            R.drawable.data_037,
+            R.drawable.data_038,
+            R.drawable.data_039,
+            R.drawable.data_040,
+            R.drawable.data_041,
+            R.drawable.data_042,
+            R.drawable.data_043,
+            R.drawable.data_045,
+            R.drawable.data_046,
+            R.drawable.data_047,
+            R.drawable.data_048,
+            R.drawable.data_049,
+            R.drawable.data_050,
+            R.drawable.data_051,
+    };
 
 
     @Override
@@ -40,45 +82,42 @@ public class SplashScreen extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
 
-        Button testbutton = (Button) findViewById(R.id.testbutton);
-        TextView testtext = (TextView) findViewById(R.id.testtext);
-        animationview = (ImageView) findViewById(R.id.animationview);
+
+        ImageView animationview = (ImageView) findViewById(R.id.animationview);
 
 
-        // Drawable d = getDrawable(R.drawable.logov3);
-        //if (d instanceof AnimatedVectorDrawable) {
-        //  AnimatedVectorDrawable avd  = (AnimatedVectorDrawable) d;
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Update the ImageView with the next image
 
-        //  testtext.setText("1");
-        //  avd.start();
 
-        // }
+                animationview.setBackgroundResource(imageResources[currentImageIndex]);
 
-    }
+                currentImageIndex = (currentImageIndex + 1) % imageResources.length;
 
-        @Override
-        protected void onStart() {
-            super.onStart();
-
-            Drawable d = animationview.getDrawable();
-            if (d instanceof AnimatedVectorDrawable) {
-                AnimatedVectorDrawable anim = (AnimatedVectorDrawable) d;
-                anim.start();
-                anim.registerAnimationCallback(new Animatable2.AnimationCallback() {
-                    @Override
-                    public void onAnimationEnd(Drawable drawable) {
-                        anim.start();
-                    }
-                });
+                // Repeat every second
+                handler.postDelayed(this, 70);
             }
-        Handler splashHandler = new Handler();
+
+        };
+        // Start the image switching loop
+        handler.post(runnable);
+
+
+
+
+
+
+        Handler splashHandleractivity = new Handler();
 // Delay for 2 seconds (2000 milliseconds)
-        splashHandler.postDelayed(() -> {
+        splashHandleractivity.postDelayed(() -> {
             // Start the main activity
             Intent intent = new Intent(SplashScreen.this, MainActivity.class);
             startActivity(intent);
             finish(); // Close the splash screen activity
-        }, 2000); // Adjust the delay as needed
+        }, 1600); // Adjust the delay as needed
     }
 
 }
